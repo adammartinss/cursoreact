1e87456ef80de10e83f9587b64ee593e03036d62
adammartinss <103488271+adammartinss@users.noreply.github.com>
2022-05-10 22:03:48 -0300

    Revert "Merge branch 'main' of https://github.com/adammartinss/cursoreact"
    This reverts commit 38f3b3b6639cb082f133c571307aba5f6ee534ab, reversing
    changes made to cbc25b468c81616c99677c6af7071310715eeb3c.

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -4,12 +4,8 @@ module.exports = {
         extend: {
             colors: {
                 brand: {
-                    300: '#996DFF',
                     500: '#8257e6',
                 },
-            },
-            borderRadius: {
-                md: '4px'
             },
         },
     },
