42394765ffa9480682adf4bcd1e941318635a136
Adam Martins Santos <103488271+adammartinss@users.noreply.github.com>
2022-05-11 09:17:57 -0300

    Revert "as"
    This reverts commit 9b14a0ff6fe75cee84c2b6c30457a6b02083b8e9.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,6 @@
+import { prisma } from './prisma'
 import express from 'express';
+import nodemailer from 'nodemailer'
 
 const app = express();
 
@@ -9,8 +11,39 @@ app.use(express.json());
 // put = atualizar info de uma entdd
 // patch = atualizar uma infounica de uma entdd
 // delete = delte
+const transport = nodemailer.createTransport({
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    auth: {
+        user: '34a21526d2c127',
+        pass: '50c09987bb4117',
+    },
+});
+app.post('/feedbacks', async (req, res) => {
+    const {type, comment, screenshot} = req.body
+    const feedback = await prisma.feedback.create({
 
-app.use(routes)
+        data:{
+            type,
+            comment,
+            screenshot,
+        }
+    })
+await transport.sendMail({
+    from: 'Equipe Feedget <oi@feedget.com',
+    to: 'Adam Martins <gamebacon7654@gmail.com>',
+    subject: 'Novo feedback',
+    html:[
+        `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+        `<p>Tipo do feedback: ${type}</p>`,
+        `<p>Comentário: ${comment}</p>`,
+        '</div>'
+    ].join('\n')
+    
+    
+})
+    return res.status(201).json({data: feedback})
+});
 
 app.listen(3333, () => {
     console.log('HTTP server running!');
